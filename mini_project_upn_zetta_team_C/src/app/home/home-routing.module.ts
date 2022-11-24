8c90fe1821597c5ede05e993a7bef54632b52dd5
ArdiSasongko <ardisasongko002@gmail.com>
2022-11-25 01:12:14 +0700

    update Homepage

--- mini_project_upn_zetta_team_C/src/app/home/home-routing.module.ts
+++ mini_project_upn_zetta_team_C/src/app/home/home-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuRoutingModule } from '../menu/menu-routing.module';
 import { HomeComponent } from './home.component';
 
 const routes: Routes = [
