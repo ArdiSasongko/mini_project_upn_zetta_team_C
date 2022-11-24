276aa9d604e92d1f6b3c7fd8f3232f09e2415e30
ArdiSasongko <ardisasongko002@gmail.com>
2022-11-24 12:46:34 +0700

    phase_1[MP_007 & MP_006]

--- mini_project_upn_zetta_team_C/src/app/app-routing.module.ts
+++ mini_project_upn_zetta_team_C/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartRoutingModule } from './cart/cart-routing.module';
+import { HomeRoutingModule } from './home/home-routing.module';
+import { LoginRoutingModule } from './login/login-routing.module';
+import { MenuRoutingModule } from './menu/menu-routing.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component:HomeRoutingModule
+  },
+  {
+    path:'menu',
+    component:MenuRoutingModule
+  },
+  {
+    path:'login',
+    component:LoginRoutingModule
+  },
+  {
+    path:'cart',
+    component:CartRoutingModule
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mini_project_upn_zetta_team_C/src/app/app.module.ts
+++ mini_project_upn_zetta_team_C/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Angular Material
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +16,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterLink,
+    //Angular Material
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
